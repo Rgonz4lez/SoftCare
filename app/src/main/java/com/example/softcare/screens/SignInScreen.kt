3258eb7e8e9c7d0dae03332f14ba7622bf2e587b
Raphaella <raphagonzalez@outlook.com>
2025-05-22 13:31:58 -0300

    Atualizações

--- app/src/main/java/com/example/softcare/screens/SignInScreen.kt
+++ app/src/main/java/com/example/softcare/screens/SignInScreen.kt
@@ -1,18 +1,14 @@
 package com.example.softcare.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.softcare.ui.theme.Purple
 import com.google.firebase.auth.FirebaseAuth
